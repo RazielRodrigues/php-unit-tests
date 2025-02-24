ec4431f9ebf7f51977c06a6c25246b9f5f28d9cc
Raziel Rodrigues <razielx3@live.com>
2025-02-24 17:55:21 +0000

    Create CalculatorService.php

---
+++ app/src/CalculatorService.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\src;
+
+class CalculatorService
+{
+    
+    function sum(int $x, int $y): int
+    {
+        return $x + $y;
+    }
+
+    function division(int $x, int $y): int
+    {
+
+        if ($x <= 0) {
+            throw new \Exception("Error Processing Request", 1);
+        }
+
+        return $x / $y;
+    }
+
+}
